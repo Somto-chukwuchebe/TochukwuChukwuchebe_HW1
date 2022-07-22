4bfc79a0f2744e42d2910f4ba5fddb19e469e1a8
Somto-chukwuchebe <108034646+Somto-chukwuchebe@users.noreply.github.com>
2022-07-22 23:50:44 +0300

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,9 @@
+#task 2
+#Write a program that reads from standard input, one integer number per line, and after the first zero entered, outputs the sum of the numbers received as input.
+
+first = int(input("Input first number: "))
+second = int(input("Input second number: "))
+third = int(input("Input third number: "))
+fourth = int(input("Input fourth number: "))
+fifth = int(input("Input fifth number: "))
+
---
+++ task_1.py
@@ -0,0 +1,13 @@
+#task 1
+
+first = int(input("Input first number: "))
+second = int(input("Input second number: "))
+third = int(input("Input third number: "))
+
+maximum = max(first, second, third)
+minimum = min(first, second, third)
+remainder = ((first + second + third) - maximum - minimum)
+
+print("Maximum: ", maximum)
+print("Minimum: ", minimum)
+print("Remainder: ", remainder)
